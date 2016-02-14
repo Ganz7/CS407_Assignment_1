291a954dba04935f49f64ef74588b8f6e76820a7
Ganz7 <ganzse7en@gmail.com>
2016-02-13 21:09:42 -0600

    Call PlayQuizActivity from MainActivity

--- app/src/main/java/com/wisc/ganz/quizx/MainActivity.java
+++ app/src/main/java/com/wisc/ganz/quizx/MainActivity.java
@@ -2,6 +2,8 @@ package com.wisc.ganz.quizx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void playPressed(View view){
+        Intent playQuiz = new Intent(this, PlayQuizActivity.class);
+        startActivity(playQuiz);
     }
 }
