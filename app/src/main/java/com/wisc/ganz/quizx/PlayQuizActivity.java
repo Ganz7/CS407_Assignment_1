b1499398911d2a68fd42105c22d95df1d72a23ae
Ganz7 <ganzse7en@gmail.com>
2016-02-13 21:06:47 -0600

    PlayQuizActivity created

---
+++ app/src/main/java/com/wisc/ganz/quizx/PlayQuizActivity.java
@@ -0,0 +1,13 @@
+package com.wisc.ganz.quizx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_quiz);
+    }
+}
