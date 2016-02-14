e7477e898309b7c25d3f12ae3a3dc210ed10bb04
Ganz7 <ganzse7en@gmail.com>
2016-02-14 00:08:51 -0600

    Image question fragment set up 1

--- app/src/main/java/com/wisc/ganz/quizx/ImageQuestionFragment.java
+++ app/src/main/java/com/wisc/ganz/quizx/ImageQuestionFragment.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -64,7 +65,13 @@ public class ImageQuestionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_question, container, false);
+        View view = inflater.inflate(R.layout.fragment_image_question, container, false);
+
+        TextView question = (TextView)view.findViewById(R.id.tv_ImgQuestion);
+        question.setText(R.string.question_img_1);
+
+        return view;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/wisc/ganz/quizx/PlayQuizActivity.java
+++ app/src/main/java/com/wisc/ganz/quizx/PlayQuizActivity.java
@@ -1,13 +1,26 @@
 package com.wisc.ganz.quizx;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PlayQuizActivity extends AppCompatActivity {
+public class PlayQuizActivity extends AppCompatActivity
+        implements ImageQuestionFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_quiz);
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.quiz_fragment_container, ImageQuestionFragment.newInstance(null, null))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void onFragmentInteraction(Uri uri){
+        //Empty for now
+        //Will expand if need to communicate between fragments
     }
 }
